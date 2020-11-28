a4032197cc845ec6477cc197d0e9eaace8af6726
devSVG <devSVG@devsteam.mobi>
2020-11-29 00:01:04 +0200

    minor fixes

---
+++ src/screens/index.ts
@@ -0,0 +1,15 @@
+import {default as MediaScreen} from './MediaScreen';
+import {default as NewsScreen} from './NewsScreen';
+import {default as QueueScreen} from './QueueScreen';
+import {default as SettingScreen} from './SettingScreen';
+import {default as TableScreen} from './TableScreen';
+import {default as TicketScreen} from './TicketScreen';
+
+export {
+  MediaScreen,
+  NewsScreen,
+  QueueScreen,
+  SettingScreen,
+  TableScreen,
+  TicketScreen,
+};
--- src/services/news.ts
+++
@@ -1,8 +0,0 @@
-import {NEWS_URI} from '../constants/uris';
-
-export const getNewsService = async (): Promise<Array<Object>> => {
-  // переписать object
-  const response = await fetch(NEWS_URI);
-  const result = await response.json();
-  return result.rows;
-};
